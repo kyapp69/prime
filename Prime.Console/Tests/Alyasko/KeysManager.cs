186319fd976ba235312c9246192e923c14fc041b
Alyasko <yasko.alexander@gmail.com>
2018-03-06 22:44:23 +0200

    Console app minor changes.

---
+++ Prime.Console/Tests/Alyasko/KeysManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using Prime.Common;
+using Prime.Utility;
+
+namespace Prime.Console.Tests.Alyasko
+{
+    public class KeysManager : ITestBase
+    {
+        public void Go()
+        {
+            System.Console.WriteLine("--- Keys Management Tool ---");
+
+            var providers = Networks.I.Providers.AsEnumerable().FilterType<INetworkProvider, INetworkProviderPrivate>().Where(x => x.IsDirect).ToList();
+
+            foreach (var provider in providers)
+            {
+                System.Console.WriteLine($"{provider.Network.Name}");
+                var providerKey = UserContext.Current.GetApiKey(provider);
+                if(providerKey != null)
+                    System.Console.WriteLine($"Key: {providerKey.Key}");
+
+                System.Console.WriteLine();
+            }
+
+            System.Console.WriteLine("Enter exchange name to enter keys info:");
+            var exhcnageName = System.Console.ReadLine();
+
+            var exchange =
+                providers.FirstOrDefault(x => x.Network.Name.Equals(exhcnageName, StringComparison.OrdinalIgnoreCase));
+
+            if (exchange != null)
+            {
+                System.Console.WriteLine("Entering keys for: ");
+                System.Console.WriteLine($"{exchange.Title}");
+
+                System.Console.WriteLine("Enter Key:");
+                var key = System.Console.ReadLine();
+
+                System.Console.WriteLine("Enter Secret:");
+                var secret = System.Console.ReadLine();
+
+                var apiKey = new ApiKey(exchange.Network, "Default", key, secret);
+                var keys = UserContext.Current.ApiKeys;
+                keys.Remove(apiKey);
+
+                keys.Add(apiKey);
+                keys.Save();
+            }
+        }
+    }
+}
