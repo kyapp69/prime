7afe53dc42232b9f61012efe7b038482345e3cff
Alyasko <yasko.alexander@gmail.com>
2018-03-10 11:51:24 +0200

    Implemented auto UI startup (for WIN only).

--- Prime.KeysManager/Program.cs
+++ Prime.KeysManager/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading.Tasks;
 using Prime.KeysManager.Core;
 using Prime.KeysManager.Transport;
+using Prime.KeysManager.Utils;
 
 namespace Prime.KeysManager
 {
@@ -11,8 +14,26 @@ namespace Prime.KeysManager
     {
         static void Main(string[] args)
         {
-            var app = new KeysManagerApp(new TcpServer(), new PrimeService());
-            app.Run();
+            Console.WriteLine($"Current directory: " + Environment.CurrentDirectory);
+            Console.WriteLine($"UI app directory: " + ConfigManager.AppConfig.UiAppFolderPath);
+
+            // Start server.
+            var t = Task.Run(() =>
+            {
+                var app = new KeysManagerApp(new TcpServer(), new PrimeService());
+                app.Run();
+            });
+            Console.WriteLine("> Server started");
+
+            // Start UI.
+            var uiProcess = new Process();
+            uiProcess.StartInfo.FileName = "cmd";
+            uiProcess.StartInfo.Arguments = "/C npm start";
+            uiProcess.StartInfo.WorkingDirectory = ConfigManager.AppConfig.UiAppFolderPath;
+            uiProcess.Start();
+
+
+            t.Wait();
         }
     }
 }
