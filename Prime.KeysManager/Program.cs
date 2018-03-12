35f955b8c0517e590b1482ae659dfbbc2271b17d
Alyasko <yasko.alexander@gmail.com>
2018-03-12 13:29:58 +0200

    Implemented UI auto start for Unix.

--- Prime.KeysManager/Program.cs
+++ Prime.KeysManager/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -14,10 +15,21 @@ namespace Prime.KeysManager
     {
         static void Main(string[] args)
         {
+            Console.WriteLine($"Operating system: " + Environment.OSVersion.Platform);
             Console.WriteLine($"Current directory: " + Environment.CurrentDirectory);
             Console.WriteLine($"UI app directory: " + ConfigManager.AppConfig.UiAppFolderPath);
 
             // Start server.
+            var t = RunServer();
+
+            // Start UI.
+            RunUi();
+
+            t.Wait();
+        }
+
+        private static Task RunServer()
+        {
             var t = Task.Run(() =>
             {
                 var app = new KeysManagerApp(new TcpServer(), new PrimeService());
@@ -25,15 +37,31 @@ namespace Prime.KeysManager
             });
             Console.WriteLine("> Server started");
 
-            // Start UI.
+            return t;
+        }
+
+        private static void RunUi()
+        {
             var uiProcess = new Process();
-            uiProcess.StartInfo.FileName = "cmd";
-            uiProcess.StartInfo.Arguments = "/C npm start";
-            uiProcess.StartInfo.WorkingDirectory = ConfigManager.AppConfig.UiAppFolderPath;
+            
+            if (Environment.OSVersion.Platform == PlatformID.MacOSX || Environment.OSVersion.Platform == PlatformID.Unix)
+            {
+                uiProcess.StartInfo.FileName = "/bin/bash";
+                uiProcess.StartInfo.Arguments = "-c \"npm start\"";
+                uiProcess.StartInfo.WorkingDirectory = "Electron";//;
+                uiProcess.StartInfo.RedirectStandardOutput = true;
+                //uiProcess.StartInfo.RedirectStandardError = true;
+                uiProcess.StartInfo.UseShellExecute = false;
+                //uiProcess.StartInfo.CreateNoWindow = false;
+            }
+            else
+            {
+                uiProcess.StartInfo.FileName = "cmd";
+                uiProcess.StartInfo.Arguments = "/C npm start";
+                uiProcess.StartInfo.WorkingDirectory = ConfigManager.AppConfig.UiAppFolderPath;
+            }
+            
             uiProcess.Start();
-
-
-            t.Wait();
         }
     }
 }
