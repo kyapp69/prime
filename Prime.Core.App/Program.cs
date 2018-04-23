46b6afda9d95b50e0d4243d468b790035b50b494
Alyasko <yasko.alexander@gmail.com>
2018-04-23 10:54:13 +0300

    Added Prime solution. Solution architecture update.

---
+++ Prime.Core.App/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Prime.Bootstrap
+{
+    /// <summary>
+    /// Entry point of Prime.
+    /// Starts core. Core starts Radiant.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
