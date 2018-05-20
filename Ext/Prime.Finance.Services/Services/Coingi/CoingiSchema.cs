366e8a3f8ec7724ab4a7005392ee78dacf5d66e7
Sean Caruana <seancaruana@outlook.com>
2018-05-20 14:02:46 +0200

    Created Coingi schema for order books within Coingi

---
+++ Ext/Prime.Finance.Services/Services/Coingi/CoingiSchema.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Prime.Finance.Services.Services.Coingi
+{
+    internal class CoingiSchema
+    {
+        #region Public
+        internal class OrderBookResponse
+        {
+            public OrderBookItemResponse[] asks;
+            public OrderBookItemResponse[] bids;
+        }
+
+        internal class OrderBookItemResponse
+        {
+            public CurrencyPair currencyPair;
+            public decimal price;
+            public decimal baseAmount;
+            public decimal counterAmount;
+        }
+
+        internal class CurrencyPair
+        {
+            [JsonProperty("base")]
+            public string baseAsset;
+            public string counter;
+        }
+        #endregion
+    }
+}
---
+++ Ext/Prime.Finance.Services/Services/Coingi/ICoingiApi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using RestEase;
+
+namespace Prime.Finance.Services.Services.Coingi
+{
+    internal interface ICoingiApi
+    {
+        [Get("/order-book/{currencyPair}/{maxAskCount}/{maxBidCount}/{maxDepthRangeCount}")]
+        Task<CoingiSchema.OrderBookResponse> GetOrderBookAsync([Path] string currencyPair, [Path] int maxAskCount, [Path] int maxBidCount, [Path] int maxDepthRangeCount);
+    }
+}
