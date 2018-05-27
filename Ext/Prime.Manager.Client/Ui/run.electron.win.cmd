0ae8235f9a0c284376e0c5ef34b1b3d90655beeb
Alyasko <yasko.alexander@gmail.com>
2018-05-27 13:04:14 +0300

    Updated Prime.Manager scripts.

--- Ext/Prime.Manager.Client/Ui/run.electron.win.cmd
+++ Ext/Prime.Manager.Client/Ui/run.electron.win.cmd
@@ -1,1 +1,2 @@
-.\runtime\node-v8.11.2-win-x64\npm.exe run electron
+.\runtime\node-v8.11.2-win-x64\node-v8.11.2-win-x64\npm install
+.\runtime\node-v8.11.2-win-x64\node-v8.11.2-win-x64\npm run electron
