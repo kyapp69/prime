fd0442e80b862849267a21e063d456174b07a814
Alyasko <yasko.alexander@gmail.com>
2018-05-27 13:26:50 +0300

    Win startup scripts updated.

--- Ext/Prime.Manager.Client/Ui/run.angular.win.cmd
+++ Ext/Prime.Manager.Client/Ui/run.angular.win.cmd
@@ -1,2 +1,2 @@
-.\runtime\node-v8.11.2-win-x64\node-v8.11.2-win-x64\npm install
-.\runtime\node-v8.11.2-win-x64\node-v8.11.2-win-x64\npm run ng serve
+call .\runtime\node-v8.11.2-win-x64\node-v8.11.2-win-x64\npm install
+call .\runtime\node-v8.11.2-win-x64\node-v8.11.2-win-x64\npm run ng serve
--- Ext/Prime.Manager.Client/Ui/run.electron.win.cmd
+++ Ext/Prime.Manager.Client/Ui/run.electron.win.cmd
@@ -1,2 +1,2 @@
-.\runtime\node-v8.11.2-win-x64\node-v8.11.2-win-x64\npm install
-.\runtime\node-v8.11.2-win-x64\node-v8.11.2-win-x64\npm run electron
+call .\runtime\node-v8.11.2-win-x64\node-v8.11.2-win-x64\npm install
+call .\runtime\node-v8.11.2-win-x64\node-v8.11.2-win-x64\npm run electron
