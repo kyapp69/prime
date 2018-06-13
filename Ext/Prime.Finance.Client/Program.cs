acc7773e3bdd2d0e9e245d193eed5af00129e3d9
Alyasko <yasko.alexander@gmail.com>
2018-06-13 13:24:33 +0300

    ex-KeysManager moved to Prime.Finance.Client. Mid-way.

---
+++ Ext/Prime.Finance.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prime.Finance.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
