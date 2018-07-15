652988c2a3a5891695e63f22e4305fb13629ce7b
Alyasko <yasko.alexander@gmail.com>
2018-07-15 17:40:08 +0300

    Implemented sample Prime.Web project.

---
+++ Prime.Web/ClientApp/app/components/configure/configure.component.ts
@@ -0,0 +1,30 @@
+﻿import { Component, OnInit } from "@angular/core";
+import * as EventSource from 'eventsource';
+
+@Component({
+    selector: 'configure',
+    templateUrl: './configure.component.html'
+})
+export class ConfigureComponent implements OnInit {
+    serverUtcTime: string = "fetching...";
+
+    ngOnInit(): void {
+        var source = new EventSource('api/Configure/ServerTime');
+
+        source.onmessage = (event: any) => {
+            this.sseOnMessage(event);
+        };
+
+        source.onopen = function(event: any) {
+            console.log('onopen');
+        };
+
+        source.onerror = function(event: any) {
+            console.log('onerror');
+        }
+    }
+
+    private sseOnMessage(event: any) {
+        this.serverUtcTime = event.data;
+    }
+}
---
+++ Prime.Web/Controllers/ConfigureController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Prime.Web.Controllers
+{
+    [Route("/api/[controller]")]
+    public class ConfigureController : Controller
+    {
+        [HttpGet("[action]")]
+        public async Task ServerTime()
+        {
+            var response = HttpContext.Response;
+            response.Headers.Add("Content-Type", "text/event-stream");
+
+            for (var i = 0; true; ++i)
+            {
+                await response
+                    .WriteAsync($"data: {DateTime.UtcNow:HH:mm:ss tt}\r\r");
+
+                response.Body.Flush();
+                await Task.Delay(500);
+            }
+        }
+    }
+}
