de79c42c84efee0c7f9fd8bd5c8013a4944f810b
Alyasko <yasko.alexander@gmail.com>
2018-09-20 21:47:32 +0300

    Implemented PowerShell VS Code extensions export script for importing on Windows.

--- .vscode/extensions/export-extensions.win.ps1
+++ .vscode/extensions/export-extensions.win.ps1
@@ -1,5 +1,22 @@
-$ext = code --list-extensions
-for($i = 0; $i -le $ext.Length; $i++)
-{
-    Write-Host $ext[$i]
+$exts = code --list-extensions
+$commands = New-Object System.Collections.ArrayList
+$realCount = 0
+for ($i = 0; $i -le $exts.Length; $i++) {
+    $ext = $exts[$i]
+    if (-Not ([string]::IsNullOrWhiteSpace($ext)) -and ($ext -notlike "*createInstance*")) {
+        $command = "call code --install-extension " + $exts[$i]
+        $commands.Add($command) > $null
+        $realCount++
+    }
 }
+
+Write-Host "Total extensions: " $realCount
+
+$fileTag = Read-Host "Enter your name (will be used as tag for output file name)"
+
+$outputFile = "import/import-extensions.$fileTag.win.cmd"
+Write-Host "Saving to '$outputFile'."
+
+Out-File -FilePath $outputFile -InputObject $commands -Encoding utf8
+Write-Host "File saved."
+
